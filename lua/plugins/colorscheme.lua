8daea9e51bfea31573f6e8787198c9f7aac838c1
nobb <zyr3536@163.com>
2024-02-14 15:26:48 +0800

    init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
+}
